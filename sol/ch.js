180c397e632ec11d5a8899a95f21f25bf9d44af5
Samuel Huang <enminghuang21119@gmail.com>
2021-01-04 14:18:24 +0800

    More flexible for multi chapters & CH. 17

---
+++ sol/ch.js
@@ -0,0 +1,13 @@
+const availableCh = [15, 16, 17];
+
+(() => {
+  const dropdown = document.getElementById("dropdown1");
+  availableCh.forEach(ch => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.setAttribute("href", `ch${ch}.html`);
+    a.innerText = `CH. ${ch}`;
+    li.appendChild(a);
+    dropdown.appendChild(li);
+  });
+})();
