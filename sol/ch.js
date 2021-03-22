d95709f69a8c6b9e89e5f4903d50eb886a46f668
Samuel Huang <enminghuang21119@gmail.com>
2021-03-22 22:11:01 +0800

    2021 Spring Mid.1

--- sol/ch.js
+++ sol/ch.js
@@ -1,4 +1,4 @@
-const availableCh = [15, 16, 17, 18, 19, 20, 21, 22];
+const availableCh = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 
 (() => {
   const dropdown = document.getElementById("dropdown1");
