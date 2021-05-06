4dc28a8a130d824150f5bc0c5bce03cab49e7e5a
Samuel Huang <enminghuang21119@gmail.com>
2021-05-06 20:39:06 +0800

    CH 27

--- ch.js
+++ ch.js
@@ -1,4 +1,4 @@
-const availableCh = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+const availableCh = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27];
 
 (() => {
   const chs = document.getElementById("chs");
